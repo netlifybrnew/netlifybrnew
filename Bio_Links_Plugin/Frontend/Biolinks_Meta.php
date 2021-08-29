576969e91fb72a37f3ae66d53c09d2984165d04a
netlifybrnew <126541@gmail.com>
2021-08-29 16:17:02 +0530

    Updated repo

---
+++ Bio_Links_Plugin/Frontend/Biolinks_Meta.php
@@ -0,0 +1,93 @@
+<?php
+
+
+namespace Bio_Links_Plugin\Frontend;
+
+
+class Biolinks_Meta {
+
+	protected $post_id;
+
+	/**
+	 * @var Meta
+	 */
+	protected $meta;
+
+	/**
+	 * Biolinks_Meta constructor.
+	 */
+	public function __construct( $post_id ) {
+
+		$this->post_id = $post_id;
+		$this->meta    = new Meta( $post_id );
+
+	}
+
+
+	public function the_thumbnail() {
+
+
+		if ( ! $this->meta->get( 'thumbnail_id' ) ) {
+			return;
+		}
+
+
+		$img = wp_get_attachment_image(
+			$this->meta->get( 'thumbnail_id' ),
+			'thumbnail',
+			NULL,
+			[
+				'class' => 'thumb',
+			]
+		);
+
+		echo $img;
+
+
+	}
+
+
+	public function the_description() {
+
+		if ( ! $this->meta->get( 'description' ) ) {
+			return;
+		}
+
+		echo wpautop( wp_kses_post( $this->meta->get( 'description' ) ) );
+
+	}
+
+
+	/**
+	 * @return Link[]
+	 */
+	public function links() {
+
+		$links = $this->meta->get( 'links' );
+
+		if ( empty( $links ) || ! is_array( $links ) ) {
+			return [];
+		}
+
+		$results = [];
+		foreach ( $links as $link_meta ) {
+			$results[] = new Link( $link_meta );
+		}
+
+		return $results;
+
+	}
+
+
+	public function layout() {
+
+		if ( ! $this->meta->get( 'layout' ) ) {
+			return 'default';
+		}
+
+		return $this->meta->get( 'layout' );
+
+	}
+
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Bio Links - WordPress Plugin
+
+With Bio Links plugin you can turn a single link into many.  (for example, in your Instagram Profile Bio). A helpful tool direct your visitors where they need to go.
